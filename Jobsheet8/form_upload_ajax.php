afdb2aaa2fadd88b39f685562b1eb7a4753821b5
LAPTOP-RBLOQQKD\Naswa Nida <naswanida3@gmail.com>
2025-11-05 14:48:59 +0700

    Jobsheet 8

---
+++ Jobsheet8/form_upload_ajax.php
@@ -0,0 +1,42 @@
+<!-- <!DOCTYPE html>
+<html>
+    <head>
+        <title>Unggah File Dokumen</title>
+    </head>
+    <body>
+        <form id="upload-form" action="upload_ajax.php" method="post" enctype="multipart/form-data">
+            <input type="file" name="file" id="file" multiple="multiple">
+            <input type="submit" name="submit" value="Unggah">
+        </form>
+        <div id="status"></div>
+
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script src="upload.js"></script>
+    </body>
+</html> -->
+
+<!-- modif no 1 prak 4 -->
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="upload.css">
+        <title>Unggah File Dokumen</title>
+    </head>
+    <body>
+        <div class="upload-form-container">
+            <h2>Unggah File Dokumen</h2>
+            <form id="upload-form" action="upload.php" method="post" enctype="multipart/form-data">
+                <div class="file-input-container">
+                    <input type="file" name="file" id="file" class="file-input">
+                    <label for="file" class="file-label">Pilih File</label>
+                </div>
+                <button type="submit" name="submit" class="upload-button" id="upload-button" disabled>Unggah</button>
+            </form>
+            <div id="status" class="upload-status"></div>
+        </div>
+        <footer class="footer">&copy; 2023 Naswanida Nafiula</footer>
+
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script src="upload.js"></script>
+    </body>
+</html>
